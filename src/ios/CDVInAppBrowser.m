dpa99c/cordova-plugin-inappbrowser-popup-bridge

commit 4573c87d0b74c087ef35e40f4311674bc92e7947

Fix showing/hiding of IAB on iOS

src/ios/CDVInAppBrowser.m
@@ -206,22 +206,17 @@
     
     [self.inAppBrowserViewController navigateTo:url];
     [self show:nil withNoAnimate:browserOptions.hidden];
-    if (browserOptions.hidden) {
-        self.inAppBrowserViewController.view.hidden = YES;
-    }
-    
 }
 
 - (void)show:(CDVInvokedUrlCommand*)command{
-    [self show:nil withNoAnimate:NO];
+    [self show:command withNoAnimate:NO];
 }
 
 - (void)show:(CDVInvokedUrlCommand*)command withNoAnimate:(BOOL)noAnimate
 {
-    BOOL wasHidden = self.inAppBrowserViewController.view.hidden;
-    if(wasHidden){
-        self.inAppBrowserViewController.view.hidden = NO;
-        noAnimate = YES;
+    BOOL initHidden = NO;
+    if(command == nil && noAnimate == YES){
+        initHidden = YES;
     }
     
     if (self.inAppBrowserViewController == nil) {
@@ -233,7 +228,9 @@
         return;
     }
     
-    _previousStatusBarStyle = [UIApplication sharedApplication].statusBarStyle;
+    if(!initHidden){
+        _previousStatusBarStyle = [UIApplication sharedApplication].statusBarStyle;
+    }
     
     __block CDVInAppBrowserNavigationController* nav = [[CDVInAppBrowserNavigationController alloc]
                                                         initWithRootViewController:self.inAppBrowserViewController];
@@ -246,7 +243,13 @@
     // Run later to avoid the "took a long time" log message.
     dispatch_async(dispatch_get_main_queue(), ^{
         if (weakSelf.inAppBrowserViewController != nil) {
-            CGRect frame = [[UIScreen mainScreen] bounds];
+            CGRect frame;
+            if(initHidden){
+                frame = CGRectMake(0, 0, 0, 0);
+            }else{
+                frame = [[UIScreen mainScreen] bounds];
+            }
+            
             UIWindow *tmpWindow = [[UIWindow alloc] initWithFrame:frame];
             UIViewController *tmpController = [[UIViewController alloc] init];
             [tmpWindow setRootViewController:tmpController];
@@ -277,7 +280,7 @@
     dispatch_async(dispatch_get_main_queue(), ^{
         if (self.inAppBrowserViewController != nil) {
             _previousStatusBarStyle = -1;
-            [self.viewController dismissViewControllerAnimated:YES completion:nil];
+            [self.inAppBrowserViewController.presentingViewController dismissViewControllerAnimated:YES completion:nil];
         }
     });
 }

dpa99c/cordova-plugin-inappbrowser-wkwebview

commit 1792135c5fec5faa95d30282348e9cb1a0c389bd

Fix content sizing bug caused by 4573c87d0b74c087ef35e40f4311674bc92e7947

src/ios/CDVInAppBrowser.m
@@ -243,11 +243,9 @@
     // Run later to avoid the "took a long time" log message.
     dispatch_async(dispatch_get_main_queue(), ^{
         if (weakSelf.inAppBrowserViewController != nil) {
-            CGRect frame;
+            CGRect frame = [[UIScreen mainScreen] bounds];
             if(initHidden){
-                frame = CGRectMake(0, 0, 0, 0);
-            }else{
-                frame = [[UIScreen mainScreen] bounds];
+                frame.origin.x = -10000;
             }
             
             UIWindow *tmpWindow = [[UIWindow alloc] initWithFrame:frame];
@@ -903,6 +901,10 @@ BOOL isExiting = FALSE;
 {
     if (IsAtLeastiOSVersion(@"7.0") && !viewRenderedAtLeastOnce) {
         viewRenderedAtLeastOnce = TRUE;
+        CGRect viewBounds = [self.webView bounds];
+        viewBounds.origin.y = STATUSBAR_HEIGHT;
+        viewBounds.size.height = viewBounds.size.height - STATUSBAR_HEIGHT;
+        self.webView.frame = viewBounds;
         [[UIApplication sharedApplication] setStatusBarStyle:[self preferredStatusBarStyle]];
     }
     [self rePositionViews];