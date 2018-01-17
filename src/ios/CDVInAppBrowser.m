414f54673280e4e66ce7df4749f8fc0ddb4e4a64
Dave Alden <dpa99c@gmail.com>
2018-01-17 13:26:50 +0000

    Make hiding mechanism conditional depending on iOS version. Since the frame.origin method works fine on iOS 9&10 but causes native input issues (e.g. date picket) on iOS 11. And the makeKeyAndVisible method works fine on iOS 11 but causes content sizing issues on iOS 9&10.

--- src/ios/CDVInAppBrowser.m
+++ src/ios/CDVInAppBrowser.m
@@ -243,7 +243,11 @@
     // Run later to avoid the "took a long time" log message.
     dispatch_async(dispatch_get_main_queue(), ^{
         if (weakSelf.inAppBrowserViewController != nil) {
+            float osVersion = [[[UIDevice currentDevice] systemVersion] floatValue];
             CGRect frame = [[UIScreen mainScreen] bounds];
+            if(initHidden && osVersion < 11){
+                frame.origin.x = -10000;
+            }
             
             UIWindow *tmpWindow = [[UIWindow alloc] initWithFrame:frame];
             UIViewController *tmpController = [[UIViewController alloc] init];
@@ -251,7 +255,7 @@
             [tmpWindow setRootViewController:tmpController];
             [tmpWindow setWindowLevel:UIWindowLevelNormal];
             
-            if(!initHidden){
+            if(!initHidden || osVersion < 11){
             [tmpWindow makeKeyAndVisible];
             }
             [tmpController presentViewController:nav animated:!noAnimate completion:nil];
